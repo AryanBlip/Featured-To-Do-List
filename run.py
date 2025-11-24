7f65470ebf90453829cbfa4b076f05c21af72ba6
Aryan Bhandari <aryan.bhandari2706@gmail.com>
2025-11-24 14:58:04 +0530

    Add files via upload

--- run.py
+++ run.py
@@ -6,16 +6,15 @@ from pyngrok import ngrok
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
-discordToken = os.getenv('DISCORD_TOKEN')
-ngrokToken = os.getenv('NGROK_TOKEN')
-ownerId = int(os.getenv('OWNER_ID_OF_DEV'))
-
-ngrok.set_auth_token(ngrokToken)
-public_url = ngrok.connect(5000)
-print("ngrok URL:", public_url)
-
-flask_app = create_app()
-
 if __name__ == '__main__':
+    load_dotenv()
+    discordToken = os.getenv('DISCORD_TOKEN')
+    ngrokToken = os.getenv('NGROK_TOKEN')
+    ownerId = int(os.getenv('OWNER_ID_OF_DEV'))
+
+    ngrok.set_auth_token(ngrokToken)
+    public_url = ngrok.connect(5000)
+    print("ngrok URL:", public_url)
+
+    flask_app = create_app()
     flask_app.run(host='0.0.0.0', port=5000)
